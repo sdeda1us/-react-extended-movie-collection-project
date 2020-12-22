ff156a8118622a856be3cedaed55b942940a0c1b
Steven Maloney <steven.maloney@gmail.com>
2020-12-22 12:32:14 -0600

    routes in nav bar function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,22 @@
-
+import React from 'react';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import Header from '../Header/Header';
+import ManageGenres from '../ManageGenres/ManageGenres';
+import AddBook from '../AddBook/AddBook';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Router>
+        <nav>
+              <Link to = '/'>Add Entry</Link>
+              <Link to = '/genres'>Manage Genres</Link>
+        </nav>
+          <Route exact path="/" component={AddBook}/>
+          <Route path="/genres" component={ManageGenres}/>
+      </Router>
       <p>Hi from app!</p>
     </div>
   );
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,10 +2,7 @@ import React from "React";
 
 export default function NavBar() {
     return(
-        <nav>
-            <Link to = '/'>Add Entry</Link>
-            <Link to = '/'>Manage Genres</Link>
-        </nav>
+        
     );
 
 }
