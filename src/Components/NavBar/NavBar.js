7028a1c823cfbbbe3eb44522f27512db99efdd34
Steven Maloney <steven.maloney@gmail.com>
2020-12-22 12:18:12 -0600

    built out file structure

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from "React";
+
+export default function NavBar() {
+    return(
+        <nav>
+            <Link to = '/'>Add Entry</Link>
+            <Link to = '/'>Manage Genres</Link>
+        </nav>
+    );
+
+}
